d3167b58531cf381fe4a028cd5cc609c4a2a8fef
Aaron <aaronstillmann@gmail.com>
2020-02-23 15:16:44 -0800

    added multiply and divide

--- test2.py
+++ test2.py
@@ -4,5 +4,12 @@ def sum(number1, number2):
 def subtract(number1, number2):
     return number1 - number2
 
+def multiply(number1, number2):
+    return number1 * number2
 
+def divide(number1, number2):
+    return number1 * number2
+
+
+print(multiply(9, 5))
 print(sum(11, 9))
